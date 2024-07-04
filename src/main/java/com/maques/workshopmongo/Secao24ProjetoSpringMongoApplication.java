b7ff09cf6eba7b1ef727d1ca664739e32fa8193e
wagner marques <wagenrfeitosa2000@hotmail.com>
2024-07-04 08:06:40 -0300

    primeiro commit projeto criado

---
+++ src/main/java/com/maques/workshopmongo/Secao24ProjetoSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.maques.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secao24ProjetoSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Secao24ProjetoSpringMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maques/workshopmongo/Secao24ProjetoSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maques.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secao24ProjetoSpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
